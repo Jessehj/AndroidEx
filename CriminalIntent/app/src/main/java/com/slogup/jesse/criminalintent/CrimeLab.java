46f1b639cf1461337e5f046e12f491590527e63d
Jessehj <jessehj6109@gmail.com>
2016-11-09 14:35:00 +0900

    Android Exercise Chapter.13 CriminalIntent  - The Toolbar

--- CriminalIntent/app/src/main/java/com/slogup/jesse/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/slogup/jesse/criminalintent/CrimeLab.java
@@ -28,15 +28,10 @@ public class CrimeLab {
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
 
-        for (int i = 0; i < 100; i++) {
+    }
 
-            Crime crime = new Crime();
-
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0);  // 짝수 번째 요소에 true 임의 설정.
-
-            mCrimes.add(crime);
-        }
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
     }
 
     public List<Crime> getCrimes() {
@@ -53,4 +48,5 @@ public class CrimeLab {
 
         return null;
     }
+
 }
