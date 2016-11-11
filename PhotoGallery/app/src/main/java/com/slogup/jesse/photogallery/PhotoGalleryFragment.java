81c9bbb45c3a0096c61129614854d5784549ca31
Jessehj <jessehj6109@gmail.com>
2016-11-11 16:21:07 +0900

    Android Exercise Chapter.27 PhotoGallery  - Broadcast Intents

--- PhotoGallery/app/src/main/java/com/slogup/jesse/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/app/src/main/java/com/slogup/jesse/photogallery/PhotoGalleryFragment.java
@@ -27,7 +27,7 @@ import java.util.List;
  * Created by Jesse on 2016. 11. 10..
  */
 
-public class PhotoGalleryFragment extends Fragment {
+public class PhotoGalleryFragment extends VisibleFragment {
 
     private static final String TAG = "PhotoGalleryFragment";
 
